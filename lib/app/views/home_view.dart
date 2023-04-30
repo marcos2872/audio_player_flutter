06ddfabb6338de70282f569dfe9aa03e12ca7c38
Marcos Brito <njsudev5@hotmail.com>
2023-04-30 14:54:44 -0300

    testando payer de audio

--- lib/app/views/home_view.dart
+++ lib/app/views/home_view.dart
@@ -1,10 +1,58 @@
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget {
-const HomeView({ Key? key }) : super(key: key);
+import 'package:audioplayers/audioplayers.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-    return Container();
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  AudioPlayer player = AudioPlayer();
+  final url =
+      'https://cdn.pixabay.com/download/audio/2021/10/20/audio_9c4a83fbe5.mp3?filename=child39s-play-trap-9660.mp3';
+
+  @override
+  void initState() async {
+    super.initState();
+    await player.setSourceUrl(url);
   }
+
+  @override
+  void dispose() async {
+    await player.stop();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            IconButton(
+                onPressed: () async {
+                  await player.resume();
+                },
+                icon: Icon(Icons.play_arrow)),
+            IconButton(
+                onPressed: () async {
+                  await player.pause();
+                },
+                icon: Icon(Icons.pause)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+enum PopupAction {
+  add,
+  remove,
 }
